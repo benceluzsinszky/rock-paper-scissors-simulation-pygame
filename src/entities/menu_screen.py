fb2db42eee3a3a2153b9da287794cbaaa47e5685
bence <benceluzsinszky@gmail.com>
2023-10-20 14:44:39 +0200

    Documenting menu_screen.py

--- src/entities/menu_screen.py
+++ src/entities/menu_screen.py
@@ -1,3 +1,8 @@
+"""
+This module defines the `MenuScreen` class, which represents the main menu in the game.
+It extends the `TextScreen` class and includes methods for setting up sliders, retrieving slider values,
+and managing the display of menu elements.
+"""
 import pygame
 import pygame_widgets
 from pygame_widgets.slider import Slider
@@ -5,7 +10,17 @@ from src.entities.text_screen import TextScreen
 
 
 class MenuScreen(TextScreen):
+    """
+    A class for managing the menu screen in a game.
+    """
+
     def __init__(self, screen):
+        """
+        Initialize a `MenuScreen` object with the provided Pygame screen.
+
+        Args:
+            screen (pygame.Surface): The Pygame surface representing the game screen.
+        """
         super().__init__(screen)
         self.current_screen = "menu"
         self.screen = screen
@@ -17,12 +32,28 @@ class MenuScreen(TextScreen):
         )
 
     def get_speed(self):
+        """
+        Get the current value of the speed slider.
+
+        Returns:
+            float: The selected speed value.
+        """
         return self.speed_slider.getValue()
 
     def get_group_size(self):
+        """
+        Get the current value of the group size slider.
+
+        Returns:
+            int: The selected group size value.
+        """
         return self.group_slider.getValue()
 
     def sub_loop(self):
+        """
+        The method responsible for managing the menu screen's content and interactions.
+        Displays slider values and handles button interactions.
+        """
         speed = self.get_speed()
         group_size = self.get_group_size()
         self.blit_text(f"Speed: {speed}", 185)
@@ -34,6 +65,17 @@ class MenuScreen(TextScreen):
         pygame_widgets.update(pygame.event.get())
 
     def set_up_slider(self, y_coordinate, max_value, initial_value):
+        """
+        Set up a slider with specified attributes and return it.
+
+        Args:
+            y_coordinate (int): The y-coordinate of the slider.
+            max_value (int): The maximum value the slider can reach.
+            initial_value (int): The initial value of the slider.
+
+        Returns:
+            Slider: The created slider object.
+        """
         slider = Slider(
             self.screen,
             100,
@@ -50,6 +92,9 @@ class MenuScreen(TextScreen):
         return slider
 
     def play_button(self):
+        """
+        Handle the "PLAY" button interaction by hiding sliders and transitioning to the game screen.
+        """
         self.speed_slider.hide()
         self.group_slider.hide()
         self.current_screen = "game"
