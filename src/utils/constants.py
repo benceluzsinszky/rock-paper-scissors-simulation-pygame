62d93935dfeb5c1113756bb3218005bec29b7019
bence <benceluzsinszky@gmail.com>
2023-10-20 14:39:13 +0200

    Documenting constants

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,11 +1,19 @@
+"""
+This module defines a set of constants commonly used in the application.
+"""
+
+# Screen resolution
 RESOLUTION = 500
 
+# Background color
 BGCOLOR = (255, 249, 222)
 
+# Color for contender groups
 ROCK_COLOR = (166, 208, 221)
 PAPER_COLOR = (255, 211, 176)
 SCISSORS_COLOR = (255, 105, 105)
 
+# Text attributes
 TEXTCOLOR = (43, 57, 61)
 TEXTCOLOR_HIGHLIGHTED = (75, 100, 110)
 FONT = "assets/font/PressStart2P-vaV7.ttf"
