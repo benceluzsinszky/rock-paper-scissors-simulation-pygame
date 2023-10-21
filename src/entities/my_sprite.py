6b881dadca33a88603e2d6df8ca5737f868900fa
bence <benceluzsinszky@gmail.com>
2023-10-21 23:53:47 +0200

    Documenting my_sprite

--- src/entities/my_sprite.py
+++ src/entities/my_sprite.py
@@ -1,3 +1,8 @@
+"""
+This module defines the `MySprite` class, which represents a Rock, Paper or Scissor sprite in the game.
+It extends the `Sprite` class from the `pygame.sprite` module and
+includes methods for sprite behavior, movement, and interactions.
+"""
 import math
 import random
 import pygame
@@ -7,48 +12,103 @@ from src.utils import constants
 
 class MySprite(Sprite):
     """
-    Class that creates a Rock/Paper/Scissor sprite.
+    A class for managing Rock, Paper, Scissor sprites in the game.
     """
 
     def __init__(
         self, screen, image, location, speed, own_group, hunter_group, prey_group
     ):
         """
-        Initializes a single sprite.
+        Initialize a single sprite with the provided parameters.
+
+        Args:
+            screen (pygame.Surface): The Pygame surface representing the game screen.
+            image (str): The type of sprite (rock, paper, or scissors).
+            location (tuple): The initial location of the sprite (x, y).
+            speed (float): The speed at which the sprite moves.
+            own_group (pygame.sprite.Group): The sprite's own group for tracking.
+            hunter_group (pygame.sprite.Group): The group of sprites to avoid.
+            prey_group (pygame.sprite.Group): The group of sprites to target.
         """
         super().__init__()
         self.screen = screen
         self.sprite_text = image
         self.image = pygame.image.load(f"assets/sprites/{self.sprite_text}.png")
+        self.size = 15
         self.rect = self.create_rect()
         self.rect.x = location[0]
         self.rect.y = location[1]
         self.speed = speed
-        self.size = 15
         self.own_group = own_group
         self.hunter_group = hunter_group
         self.prey_group = prey_group
 
     def get_coordinates(self):
+        """
+        Get the current coordinates of the sprite.
+
+        Returns:
+            tuple: A tuple containing the x and y coordinates of the sprite.
+        """
         return (self.rect.x, self.rect.y)
 
     def get_rect(self):
+        """
+        Get the pygame rect of the sprite.
+
+        Returns:
+            pygame.Rect: The pygame Rect representing the sprite's position and dimensions.
+        """
         return self.rect
 
     def get_sprite_text(self):
+        """
+        Get the type of the sprite (rock, paper, or scissors).
+        Used to load the image of the sprite and for the initialization of a `GameOverScreen` object.
+
+        Returns:
+            str: The type of the sprite.
+        """
         return self.sprite_text
 
     def get_coordinate_distances(self, sprite):
+        """
+        Calculate the distance between this sprite and another sprite in terms of x and y coordinates.
+
+        Args:
+            sprite (MySprite): The other sprite for which to calculate the distance.
+
+        Returns:
+            tuple: A tuple containing the distance in x and y directions.
+        """
         sprite_coordinates = sprite.get_coordinates()
         distance_x = sprite_coordinates[0] - self.rect.x
         distance_y = sprite_coordinates[1] - self.rect.y
         return distance_x, distance_y
 
     def get_distance(self, sprite):
+        """
+        Calculate the Euclidean distance between this sprite and another sprite.
+
+        Args:
+            sprite (MySprite): The other sprite for which to calculate the distance.
+
+        Returns:
+            float: The distance between the two sprites.
+        """
         distance_x, distance_y = self.get_coordinate_distances(sprite)
         return math.sqrt(distance_x**2 + distance_y**2)
 
     def get_closest(self, group):
+        """
+        Find the closest sprite within a group to this sprite.
+
+        Args:
+            group (pygame.sprite.Group): The group of sprites to search for the closest one.
+
+        Returns:
+            MySprite: The closest sprite found in the group.
+        """
         closest_sprite = None
         closest_distance = float("inf")
         for sprite in group:
@@ -59,26 +119,52 @@ class MySprite(Sprite):
         return closest_sprite
 
     def set_groups(self, own_group, hunter_group, prey_group):
+        """
+        Set the groups for this sprite.
+
+        Args:
+            own_group (pygame.sprite.Group): The sprite's own group for tracking.
+            hunter_group (pygame.sprite.Group): The group of sprites to avoid.
+            prey_group (pygame.sprite.Group): The group of sprites to target.
+        """
         self.own_group = own_group
         self.hunter_group = hunter_group
         self.prey_group = prey_group
 
     def set_image(self, image):
+        """
+        Set the sprite's image.
+
+        Args:
+            image (pygame.Surface): The new image for the sprite.
+        """
         self.image = image
         self.create_rect()
 
     def create_rect(self):
+        """
+        Create a pygame rect for the sprite based on its image.
+
+        Returns:
+            pygame.Rect: The pygame Rect representing the sprite's position and dimensions.
+        """
         self.image.convert_alpha()
-        self.image = pygame.transform.scale(self.image, (15, 15))
+        self.image = pygame.transform.scale(self.image, (self.size, self.size))
         self.image.set_colorkey((255, 255, 255))
         return self.image.get_rect()
 
     def update_sprite(self):
+        """
+        Update the sprite's position and behavior.
+        """
         self.move_sprite()
         self.self_collision()
         self.check_walls()
 
     def move_sprite(self):
+        """
+        Move the sprite based on its interactions with other sprites.
+        """
         closest_prey = self.get_closest(self.prey_group)
         if closest_prey:
             self.chase(closest_prey, 1)
@@ -90,6 +176,13 @@ class MySprite(Sprite):
                 self.chase(closest_hunter, -0.95)
 
     def chase(self, sprite, direction):
+        """
+        Make the sprite chase or evade another sprite.
+
+        Args:
+            sprite (MySprite): The target sprite to chase or evade.
+            direction (float): The chase direction (positive for chasing, negative for evading).
+        """
         distance = self.get_distance(sprite)
         if distance == 0:
             return
@@ -99,10 +192,25 @@ class MySprite(Sprite):
         self.rect.y += distance_y * movement
 
     def hunter_close_by(self, hunter):
+        """
+        Check if a hunter sprite is close by.
+
+        Args:
+            hunter (MySprite): The hunter sprite to check.
+
+        Returns:
+            bool: True if a hunter sprite is close by; otherwise, False.
+        """
         distance = self.get_distance(hunter)
         return distance < 200
 
     def eat(self, prey):
+        """
+        Make the sprite eat another sprite.
+
+        Args:
+            prey (MySprite): The prey sprite to eat.
+        """
         distance_x, distance_y = self.get_coordinate_distances(prey)
         hitbox = self.size * 0.7
 
@@ -123,12 +231,18 @@ class MySprite(Sprite):
         prey.kill()
 
     def check_walls(self):
+        """
+        Check for collisions with walls and make the sprite avoid them.
+        """
         self.avoid_walls()
         self.stay_inside_walls()
 
     def avoid_walls(self):
         """
-        Make sprites avoid screen borders.
+        Make the sprite avoid screen borders.
+
+        This method applies a dynamic avoidance effect to the sprite to prevent it from
+        getting too close to the screen borders.
         """
         # Only avoid walls, if sprite is in the outer quarters
         in_outer_quarter = (
@@ -149,6 +263,12 @@ class MySprite(Sprite):
     def calculate_aviodance(self, coordinate):
         """
         Calculate avoidance based on the position of the sprite.
+
+        Args:
+            coordinate (int): The coordinate value (x or y) for which to calculate avoidance.
+
+        Returns:
+            float: The avoidance factor to prevent the sprite from getting too close to walls.
         """
         # Play with avoidance_weight to make the effect weaker or stronger.
         avoidance_weight = self.speed
@@ -170,7 +290,10 @@ class MySprite(Sprite):
 
     def stay_inside_walls(self):
         """
-        Stops sprite at walls.
+        Stops the sprite from moving outside the game screen boundaries.
+
+        If the sprite's position exceeds the game screen boundaries (top, bottom, left, or right),
+        this method adjusts its position to keep it within the valid area.
         """
         if self.rect.top < 0:
             self.rect.top = 0
@@ -203,6 +326,9 @@ class MySprite(Sprite):
 
     def blit_sprite(self):
         """
-        Blits sprite to screen.
+        Renders the sprite on the game screen.
+
+        This method blits (renders) the sprite's image onto the game screen at its current position.
+        The image is placed within the boundaries of the sprite's pygame Rect.
         """
         self.screen.blit(self.image, self.rect)
